552df7f906d48144ddc9cd5c8ebb0b080975e9bc
alkharag <kharag.alex_sa@yahoo.com>
2015-02-27 10:18:01 -0800

    first commit

--- build-tmp/source/Minesweeper.java
+++ build-tmp/source/Minesweeper.java
@@ -1,6 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import de.bezier.guido.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Minesweeper extends PApplet {
 
 
-import de.bezier.guido.*;
+
+
 //Declare and initialize NUM_ROWS and NUM_COLS = 20
 private final static int NUM_ROWS = 20;
 private final static int NUM_COLS = 20;
@@ -214,3 +232,12 @@ public class MSButton
 
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Minesweeper" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
